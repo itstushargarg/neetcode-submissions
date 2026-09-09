1ff1267b5e731e2dde1a59ade991839cce28beba
Tushar Garg <itstushar2007@gmail.com>
2026-09-09 18:02:16 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        let max = [];
+        let i = 0;
+        for(; i < nums.length; i++){
+            if(i==0){
+                max[i] = nums[i];
+            }
+            else if(i==1){
+                max[i] = Math.max(nums[i], nums[i-1])
+                console.log(max[i])
+            }
+            else {
+                max[i] = Math.max(max[i-1],nums[i]+max[i-2])
+            }
+        }
+        return max[i-1];
+    }
+}
