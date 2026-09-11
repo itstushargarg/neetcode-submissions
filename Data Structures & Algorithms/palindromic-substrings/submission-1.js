5c83ab4f177eb41cd9dfee743b3dff8c4da626b0
Tushar Garg <itstushar2007@gmail.com>
2026-09-11 13:29:46 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        if(s.length == 1) return 1;
+        if(s.length == 2){
+            if(s[0] == s[1]) return 3;
+            else return 2;
+        } 
+        let pCount = 0;
+        for(let i = 0; i < s.length; i++){
+            let l = i, r = i;
+            while(l >= 0 && r <= s.length-1){
+                if(s[l] == s[r]){
+                    pCount++;
+                    l--;
+                    r++;
+                }
+                else{
+                    break;
+                }
+            }
+            l = i,
+            r = i+1;
+            while(l >= 0 && r <= s.length-1){
+                if(s[l] == s[r]){
+                    pCount++;
+                    l--;
+                    r++;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return pCount;
+    }
+}
